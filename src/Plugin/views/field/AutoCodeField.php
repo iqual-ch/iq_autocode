165e7a20d2527953273448aaccad14e93a002108
Philippe von Bergen <ph.vonbergen@iqual.ch>
2021-12-09 15:12:34 +0100

    View integration

---
+++ src/Plugin/views/field/AutoCodeField.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\iq_autocode\Plugin\views\field;
+
+use Drupal\views\ResultRow;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+
+/**
+ * A handler to provide proper displays for node qr code.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("iq_autocode")
+ */
+class AutoCodeField extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $relationship_entities = $values->_relationship_entities;
+    $value = NULL;
+
+    // First check the referenced entity.
+    if (isset($relationship_entities['node'])) {
+      $entity = $relationship_entities['node'];
+    }
+    elseif (isset($relationship_entities['taxonomy_term'])) {
+      $entity = $relationship_entities['taxonomy_term'];
+    }
+    else {
+      $entity = $values->_entity;
+    }
+
+    if ($entity->getEntityTypeId() === 'node' || $entity->getEntityTypeId() === 'taxonomy_term') {
+      $value = $entity->iq_autocode->view([
+        'type' => 'iq_autocode',
+        'label' => '',
+        'settings' => [
+          'height' => 400,
+          'width' => 400,
+        ],
+      ]);
+      $prefix = 'nc';
+
+      if ($entity->getEntityTypeId() == 'taxonomy_term') {
+        $prefix = 'tc';
+      }
+      $value['#prefix'] = '<a href="/iq_autocode/' . $prefix . '/' . base_convert($entity->id(), 10, 36) . '" target="_blank">';
+      $value['#suffix'] = '</a>';
+    }
+
+    return $value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // This function exists to override parent query function.
+    // Do nothing.
+  }
+
+}
---
+++ src/Plugin/views/field/AutoCodeShortLinkField.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\iq_autocode\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\Url;
+use Drupal\views\ResultRow;
+use Drupal\Core\Link;
+use Drupal\Core\Url as CoreUrl;
+
+/**
+ * A handler to provide proper displays for node qr code.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("iq_autocode_short_link")
+ */
+class AutoCodeShortLinkField extends Url {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue(ResultRow $values, $field = NULL) {
+    return $values->_entity->iq_autocode_short_link->uri;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $value = $this->getValue($values);
+    if (!empty($this->options['display_as_link'])) {
+      // @todo Views should expect and store a leading /. See:
+      //   https://www.drupal.org/node/2423913
+      return Link::fromTextAndUrl($this->sanitizeValue($value), CoreUrl::fromUri($value))->toString();
+    }
+    else {
+      return $this->sanitizeValue($value, 'url');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // This function exists to override parent query function.
+    // Do nothing.
+  }
+
+}
