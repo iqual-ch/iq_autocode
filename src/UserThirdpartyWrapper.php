ad466f239053242a107261cd4a160cd1e6eedc61
Philippe von Bergen <ph.vonbergen@iqual.ch>
2021-12-13 16:13:56 +0100

    Create thirdparty wrapper for user

---
+++ src/UserThirdpartyWrapper.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\iq_autocode;
+
+/**
+ *
+ */
+
+use Drupal\Core\Config\Entity\ThirdpartySettingsInterface;
+
+/**
+ *
+ */
+class UserThirdpartyWrapper implements ThirdpartySettingsInterface {
+
+  /**
+   * The config prefix.
+   *
+   * @var string
+   */
+  protected $prefix = 'user.';
+
+  /**
+   * Config keys.
+   *
+   * @var array
+   */
+  protected const KEYS = [
+    'qr_enable',
+    'qr_base_domain',
+    'qr_show',
+    'qr_show_url',
+    'qr_utm_source',
+    'qr_utm_medium',
+    'qr_utm_campaign',
+    'qr_utm_content',
+    'qr_utm_term',
+    'short_enable',
+    'short_base_domain',
+    'short_show_url',
+    'short_utm_source',
+    'short_utm_medium',
+    'short_utm_campaign',
+    'short_utm_content',
+    'short_utm_term',
+  ];
+
+  /**
+   *
+   */
+  public function __construct() {
+    $this->config = \Drupal::service('config.factory')->getEditable('iq_autocode.settings');
+  }
+
+  /**
+   *
+   */
+  public function __destruct() {
+    $this->config->save();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function setThirdPartySetting($module, $key, $value) {
+    $this->config->set($this->prefix . $key, $value);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getThirdPartySetting($module, $key, $default = NULL) {
+    $value = $this->config->get($this->prefix . $key);
+    return empty($value) ? $default : $value;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getThirdPartySettings($module) {
+    $settings = [];
+    foreach (self::KEYS as $key) {
+      $settings[$key] = $this->config->get($this->prefix . $key);
+    }
+    return $settings;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function unsetThirdPartySetting($module, $key) {
+    $this->config->set($this->prefix . $key, NULL);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getThirdPartyProviders() {
+    return [];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getEntityTypeId() {
+    return 'user';
+  }
+
+}
